5979ad32a6badef9b42f0a2a84395e1683b36fc6
nnsncl <nuniroland@gmail.com>
2021-07-12 22:59:48 +0200

    Routes structure

--- sanctum-api/routes/api.php
+++ sanctum-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,13 +16,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('products', ProductController::class);
-Route::get('/products/search/{name}', [ProductController::class, 'search']);
+// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'delete']);
+});
 
-// Route::post('/products', [ProductController::class, 'store']);
-// Route::get('/products', [ProductController::class, 'index']);
+// Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
